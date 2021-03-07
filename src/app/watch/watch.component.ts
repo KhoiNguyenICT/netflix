9650040f5770351d61e2aa48b0d82629f7adc051
iinetflix <iiiiiiinetflixiiiiiii@gmail.com>
2021-03-08 01:06:32 +0700

    create component

---
+++ src/app/watch/watch.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watch',
+  template: `
+    <p>
+      watch works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/watch/watch.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WatchComponent } from './watch.component';
+
+
+
+@NgModule({
+  declarations: [WatchComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WatchModule { }
