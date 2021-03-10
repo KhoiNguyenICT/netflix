02941cd6708054d7edbadf79901dc1206dd072df
hunglv97 <viethung30081997@gmail.com>
2021-03-10 21:35:59 +0700

    fix-ui-style

--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -3,9 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-watch',
   template: `
-    <p>
-      watch works!
-    </p>
+  <div class="bg-nfl px-2">
+  <div class="flex flex-wrap-reverse h-screen items-end lg:items-center">
+    <div class="w-full lg:w-8/12 h-2/5 lg:h-3/4 lg:max-h-96">
+      <iframe class="w-full h-full" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+      </iframe>
+    </div>
+    <div class="w-full lg:w-4/12 h-2/5 lg:h-3/4 lg:max-h-96 text-sm text-white overflow-hidden relative bg-cover bg-center"
+      style="background-image: url('https://occ-0-395-58.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABboKy7KMXcSOfoYThG9lOgDoxV_WvT_gzltHt9_SwjMhA3LuUQ0u5JF5B0u_1jyLRSs4mXPgac__7ylOb0EoZrnLN677.webp?r=363');">
+
+      <div class="p-4 absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-end">
+        <div class="film-play-info  mb-1.5">
+          <img
+            src="https://occ-0-395-58.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABZ6SgpjAZSGm-O2J4o4rmSWVWkOdQModsre9BOt1cpSO0NOpRimr_MTLuUuIPI3M5V6GXkZtRqLfOm5tApZb85BH7b_dnhOG2jyQMntsCVspxbCmM6lT1NoIXiqQsufliHGdJ1IQMUPI6OMMSizu4MGbJlDxouLAutqpzGbfVTqIaQ.webp?r=30b"
+            alt="" class="w-1/3">
+          <button
+            class="flex items-center mt-1.5 px-5 py-1 bg-white text-black text-sm rounded-sm mr-3 hover:bg-opacity-80 font-semibold"><span
+              class="mr-2 text-xs"> <i class="fas fa-play"></i></span>
+            Phát</button>
+        </div>
+        <div class="name-film bg-nfl-bottom-info-play pl-4 pr-4 pb-4 -ml-4 -mb-4 -mr-4">
+            <div class="text-lg font-sans text-white font-semibold overflow-hidden overflow-ellipsis whitespace-nowrap">Yêu kết hôn và ly dị</div>
+            <p class="text-xs mt-1 mb-2.5">Thể loại: <span>Chính kịch</span></p>
+            <p class="line-clamp-3">Luật sư tình cảm. Bác sĩ ngọt ngào. Giáo sư chu đáo. Họ có vợ ủng hộ, sự nghiệp thành công... và chuyện tình vụng trộm.</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
   `,
   styles: [
   ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,86 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      fontSize: {
+        'small-11': '11px',
+        'small-12': '12px',
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+  
+  
+      minHeight: {
+        'minContent': 'min-content',
+        'maxContent': 'max-content',
+      },
+     
+  
+      boxShadow: {
+        'nfl-1': '0 4px 6px -1px rgba(0, 0, 0, 0.7), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+  
+      extend: {
+        height: {
+          10: '2.5rem',
+          minContent: 'min-content',
+          maxContent: 'max-content',
+        },
+        padding: {
+          '13': '3.25rem',
+          '1px': '1px',
+        },
+  
+        margin: {
+          '1px': '1px',
+        },
+  
+        width: {
+          '16.667': '16.6666666667%',
+          '140': '35rem',
+        },
+  
+        minWidth: {
+          '10': '2.5rem',
+          '40': '10rem',
+        },
+  
+        textColor: {
+          'nfl-logo': '#e50914',
+        }
+      },
+  
+      backgroundImage: theme => ({
+        ...theme('colors'),
+        'nfl-header': 'linear-gradient(to bottom,rgba(0,0,0,.8) 10%,rgba(0,0,0,0))',
+        'nfl-bottom-info-play': 'linear-gradient(to top,rgba(0,0,0,.92) 25%,rgba(0,0,0,0.18))',
+      }),
+  
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'nfl': '#141414',
+      }),
     },
     variants: {
-      extend: {},
+      extend: {
+        display: ['group-hover'],
+        height: ['group-hover'],
+        scale: ['active', 'group-hover'],
+        zIndex: ['hover', 'active','group-hover'],
+        backgroundColor:['hover','active','group-hover'],
+        backgroundImage:['hover','active','group-hover']
+      },
     },
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/line-clamp'),
+    ],
 };
